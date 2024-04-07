a662ed9e6329035d2891ba79e4ff93dfd0920ee0
ByteAtATime <ByteAtATime@proton.me>
2024-04-07 05:03:39 -0700

    Remove navbar from all onboarding

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -2,27 +2,29 @@
 	import { Home, Icon, ListBullet, UserGroup, Users } from 'svelte-hero-icons';
 </script>
 
-<div class="w-64 rounded-r-2xl bg-base-300">
-	<img src="/banner.svg" alt="SwimSetter" />
+{#if !window.location.pathname.startsWith('/onboarding')}
+	<div class="w-64 rounded-r-2xl bg-base-300">
+		<img src="/banner.svg" alt="SwimSetter" />
 
-	<div class="mt-4 flex flex-col gap-y-2 px-4">
-		<a
-			class="flex items-center gap-x-1 rounded-lg px-3 py-2 text-xl transition-colors hover:bg-white/10"
-			href="/"
-		>
-			<Icon src={Home} class="w-6" /> Home
-		</a>
-		<a
-			class="flex items-center gap-x-1 rounded-lg px-3 py-2 text-xl transition-colors hover:bg-white/10"
-			href="/sets"
-		>
-			<Icon src={ListBullet} class="w-6" /> Sets
-		</a>
-		<a
-			class="flex items-center gap-x-1 rounded-lg px-3 py-2 text-xl transition-colors hover:bg-white/10"
-			href="/swimmers"
-		>
-			<Icon src={UserGroup} class="w-6" /> Home
-		</a>
+		<div class="mt-4 flex flex-col gap-y-2 px-4">
+			<a
+				class="flex items-center gap-x-1 rounded-lg px-3 py-2 text-xl transition-colors hover:bg-white/10"
+				href="/"
+			>
+				<Icon src={Home} class="w-6" /> Home
+			</a>
+			<a
+				class="flex items-center gap-x-1 rounded-lg px-3 py-2 text-xl transition-colors hover:bg-white/10"
+				href="/sets"
+			>
+				<Icon src={ListBullet} class="w-6" /> Sets
+			</a>
+			<a
+				class="flex items-center gap-x-1 rounded-lg px-3 py-2 text-xl transition-colors hover:bg-white/10"
+				href="/swimmers"
+			>
+				<Icon src={UserGroup} class="w-6" /> Home
+			</a>
+		</div>
 	</div>
-</div>
+{/if}
